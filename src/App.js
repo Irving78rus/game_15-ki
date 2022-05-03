3562c24a2acf32ec41dd2f68b8afaaf8140cb031
Irving78rus <isakov911@mail.ru>
2022-05-03 15:33:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const cellSize = 100;
+function App({ items }) {
+  const [emptyLeft, setEmptyLeft] = useState(300);
+  const [emptyTop, setEmptyTop] = useState(300);
+  const [time, setTime] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [itemsForRender, setItemsForRender] = useState([]);
+  const [newGame, setNewGame] = useState(false);
+  useEffect(() => {
+    const numbers = [...Array(15).keys()]
+      .map((key) => key + 1)
+      .sort(() => Math.random() - 0.5);
+
+    setItemsForRender(
+      items.map((item) => {
+        return {
+          id: item.id,
+          number: numbers[item.id],
+          left: (item.id % 4) * cellSize,
+          top: ((item.id - (item.id % 4)) / 4) * cellSize,
+        };
+      })
+    );
+  }, [newGame]);
+  useEffect(() => {
+    if (!isFinished) {
+      setTimeout(() => setTime(time + 1), 1000);
+      console.log(time);
+    }
+  }, [time]);
+
+  useEffect(() => {
+    setIsFinished(
+      itemsForRender.every((cell) => {
+        return cell.number === (cell.top * 4 + cell.left) / 100 + 1;
+      })
+    );
+  }, [itemsForRender]);
+  const Move = (item) => {
+    if (
+      Math.abs(item.left - emptyLeft) <= 100 &&
+      Math.abs(item.top - emptyTop) <= 100 &&
+      Math.abs(item.left + item.top - (emptyLeft + emptyTop)) === 100
+    ) {
+      setItemsForRender(
+        itemsForRender.map((u) =>
+          u.id === item.id ? { ...u, left: emptyLeft, top: emptyTop } : u
+        )
+      );
+      setEmptyTop(item.top);
+      setEmptyLeft(item.left);
+    }
+  };
+  const again = () => {
+    setIsFinished(!isFinished);
+    setNewGame(!newGame);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!isFinished && itemsForRender.length === 15 ? (
+        <div className="App">
+          {itemsForRender.map((item) => (
+            <div
+              className="cell"
+              onClick={() => {
+                Move(item);
+              }}
+              style={{ left: item.left, top: item.top }}
+            >
+              {item.number ? item.number : null}
+            </div>
+          ))}
+          <div
+            className="cell"
+            style={{ left: emptyLeft, top: emptyTop }}
+          ></div>
+        </div>
+      ) : (
+        <div>
+          {`isFinished you time ${time} sec`}
+          <button onClick={again}>начать заново</button>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/items.js
@@ -0,0 +1,87 @@
+import foto1 from './img/1.jpg';
+import foto2 from './img/2.jpg';
+import foto3 from './img/3.jpg';
+import foto4 from './img/4.jpg';
+import foto5 from './img/5.jpg';
+import foto6 from './img/6.jpg';
+import foto7 from './img/7.jpg';
+ 
+export const items = [
+  {
+    id: 0,
+    photo: foto1,
+     
+  }, 
+  {
+    id: 1,
+    photo: foto2,
+    
+  },
+  {
+    id: 2,
+    photo: foto3,
+    
+  },
+  {
+    id: 3,
+    photo: foto4,
+    
+  },
+  {
+    id: 4,
+    photo: foto5,
+    
+  },
+  {
+    id: 5,
+    photo: foto6,
+     
+  },
+  {
+    id: 6,
+    photo: foto7,
+     
+  },
+  {
+    id: 7,
+    photo: foto7,
+     
+  },
+  {
+    id: 8,
+    photo: foto7,
+     
+  },
+  {
+    id: 9,
+    photo: foto7,
+     
+  },
+  {
+    id: 10,
+    photo: foto7,
+     
+  },
+  {
+    id: 11,
+    photo: foto7,
+     
+  },
+  {
+    id: 12,
+    photo: foto7,
+     
+  },
+  {
+    id: 13,
+    photo: foto7,
+     
+  },
+  {
+    id: 14,
+    photo: foto7,
+     
+  },
+
+  
+];
